238366a058396a6f82fb647bad0468cd7089164e
Julia Sporre <juliasp@kth.se>
2016-12-01 18:00:51 +0100

    Trigger added to Game Board, Gives notification when boat is placed onto it

---
+++ Assets/Scripts/testTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Placed a boat");
+    
+    }
+
+}
