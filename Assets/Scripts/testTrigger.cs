ef4f4059daa67fe605bb2819de96c588f363bfab
Julia Sporre <juliasp@kth.se>
2016-12-09 17:19:51 +0100

    New gameboard with boxcolliders for every square. Cool low poly planes maybe for bombing the other player.

--- Assets/Scripts/testTrigger.cs
+++ Assets/Scripts/testTrigger.cs
@@ -1,12 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class testTrigger : MonoBehaviour {
 
-    void OnTriggerEnter(Collider other)
+    List<GameObject> list = new List<GameObject>();
+  
+
+void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Placed a boat");
-    
+        Debug.Log("Placed a boat" + other.name);
+
+        list.Add(other.GetComponent<GameObject>());
+
+
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Boat gone");
+        foreach (GameObject go in list)
+        {
+            if (other.GetComponent<GameObject>() == go)
+            {
+                list.Remove(go);
+            }
+        }
+        
+
     }
 
 }
